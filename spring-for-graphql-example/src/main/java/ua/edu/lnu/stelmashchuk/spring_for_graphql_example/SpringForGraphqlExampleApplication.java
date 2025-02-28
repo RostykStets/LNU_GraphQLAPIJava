5e5dfee730b5faae3c88826a888d867bbe87c584
Rostyk Stets <92711703+RostykStets@users.noreply.github.com>
2025-02-28 16:53:01 +0200

    add lecture 3 and spring with praghql usage example

---
+++ spring-for-graphql-example/src/main/java/ua/edu/lnu/stelmashchuk/spring_for_graphql_example/SpringForGraphqlExampleApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.lnu.stelmashchuk.spring_for_graphql_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForGraphqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForGraphqlExampleApplication.class, args);
+	}
+
+}
---
+++ spring-for-graphql-example/src/test/java/ua/edu/lnu/stelmashchuk/spring_for_graphql_example/SpringForGraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.lnu.stelmashchuk.spring_for_graphql_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForGraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
